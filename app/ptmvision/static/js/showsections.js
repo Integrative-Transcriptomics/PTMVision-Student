bdefca61731bc6ed7700dd8bbdc5a6a88af088ad
NetzwergDE <dominik.kempa@student.uni-tuebingen.de>
2025-11-12 15:30:13 +0100

    added some dummy section opening behavior

--- app/ptmvision/static/js/showsections.js
+++ app/ptmvision/static/js/showsections.js
@@ -1,3 +1,24 @@
 /*
 Javascript code for handling html section behavior (showing/hiding)
 */
+
+
+//show PTM Overview Section and Protein Selection Section
+function showOverviewAndProtSelect(){
+    overview = document.getElementById("OverviewSection");
+    proteinSelect = document.getElementById("ProteinSelectionSection");
+    
+    // change to visible
+    overview.style.display = "block";
+    proteinSelect.style.display = "block";
+}
+
+//show Protein Details Section
+function showProteinDetails(){
+    document.getElementById("ProteinDetailsSection").style.display = "block";
+}
+
+
+function showModificationDetails(){
+    document.getElementById("ModificationDetailsSection").style.display = "block";
+}
