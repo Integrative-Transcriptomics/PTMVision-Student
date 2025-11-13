c04d9b33888c7ca48250880b4f33f4735bfd0796
NetzwergDE <dominik.kempa@student.uni-tuebingen.de>
2025-11-13 23:24:39 +0100

    toggle checkbox for input show/hide works now :D

--- app/ptmvision/static/js/ptmvision-show-hide-elements.js
+++ app/ptmvision/static/js/ptmvision-show-hide-elements.js
@@ -30,5 +30,18 @@ function showModificationDetails(){
 Show the correct input and hide the wrong input option when selecting input methods!
 */
 function inputHider(){
+    const fstDataSet = document.getElementById("input-data-set-1");
+    const sndDataSet = document.getElementById("input-data-set-2");
+    const modeSwitch = document.getElementById("dual-mode-switch");
 
+    if(modeSwitch.checked){
+        // mode selector checked -> Enable both data input fields visible
+        sndDataSet.style.display = "block";
+    }else{
+        // mode selector unchecked -> only 1 data input field
+        sndDataSet.style.display = "none";
+
+    }
 }
+
+inputHider();
