e1eb5d4b9e24a55027b92841e17d148a39f28ebe
NetzwergDE <dominik.kempa@student.uni-tuebingen.de>
2025-11-21 10:00:49 +0100

    changed DualModeViewSelector (former: ModeSelector) display

--- app/ptmvision/static/js/ptmvision-show-hide-elements.js
+++ app/ptmvision/static/js/ptmvision-show-hide-elements.js
@@ -3,24 +3,36 @@ Javascript code for handling html behavior (showing/hiding)
 */
 
 
+/**
+ * Show/Hide the DualModeViewSelector in the Navigation Menu
+ * Default Off -> Turn On when dualMode == true
+ */
+function showDualModeViewSelector(){
+    selector = document.getElementById("DualModeViewSelector");
+    if(dualMode == true){
+        selector.style.display = "block";
+    }
+}
+
+
 //show PTM Overview Section and Protein Selection Section
-function showOverviewAndProtSelect(){
+function showOverviewAndProtSelect() {
     overview = document.getElementById("OverviewSection");
     proteinSelect = document.getElementById("ProteinSelectionSection");
-    
+
     // change to visible
     overview.style.display = "block";
     proteinSelect.style.display = "block";
 }
 
 //show Protein Details Section
-function showProteinDetails(){
+function showProteinDetails() {
     document.getElementById("ProteinDetailsSection").style.display = "block";
 }
 
 
 //show Modification Details Section
-function showModificationDetails(){
+function showModificationDetails() {
     document.getElementById("ModificationDetailsSection").style.display = "block";
 }
 
@@ -29,16 +41,16 @@ function showModificationDetails(){
 /*
 Show the correct input and hide the wrong input option when selecting input methods!
 */
-function inputHider(){
+function inputHider() {
     const sndDataSet = document.getElementById("input-data-set-2");
     const yLine = document.getElementById("input-data-y-line");
     const modeSwitch = document.getElementById("dual-mode-switch");
 
-    if(modeSwitch.checked){
+    if (modeSwitch.checked) {
         // mode selector checked -> Enable both data input fields visible
         sndDataSet.style.display = "block";
         yLine.style.display = "block";
-    }else{
+    } else {
         // mode selector unchecked -> only 1 data input field
         sndDataSet.style.display = "none";
         yLine.style.display = "none";
